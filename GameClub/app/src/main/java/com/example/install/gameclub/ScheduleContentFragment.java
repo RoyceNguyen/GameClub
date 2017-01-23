d9fe50d775fe50d0dc81da45b07d6cf3e471543f
web <web@amso0055z-07.local>
2017-01-23 09:43:36 -0500

    Added extra fragments

--- GameClub/app/src/main/java/com/example/install/gameclub/ScheduleContentFragment.java
+++ GameClub/app/src/main/java/com/example/install/gameclub/ScheduleContentFragment.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link RulesFragment.OnFragmentInteractionListener} interface
+ * {@link ScheduleContentFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link RulesFragment#newInstance} factory method to
+ * Use the {@link ScheduleContentFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class RulesFragment extends Fragment {
+public class ScheduleContentFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class RulesFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public RulesFragment() {
+    public ScheduleContentFragment() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class RulesFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment RulesFragment.
+     * @return A new instance of fragment ScheduleContentFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static RulesFragment newInstance(String param1, String param2) {
-        RulesFragment fragment = new RulesFragment();
+    public static ScheduleContentFragment newInstance(String param1, String param2) {
+        ScheduleContentFragment fragment = new ScheduleContentFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,9 +64,7 @@ public class RulesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_rules, container, false);
-
-        return view;
+        return inflater.inflate(R.layout.fragment_schedule_content, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
