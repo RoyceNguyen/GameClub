e43bdfba5ebf84ccc7c975524085ab6637160957
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2017-02-02 03:23:47 -0500

    Added the codings

--- GameClub/app/src/main/java/com/example/install/gameclub/RulesContentFragment.java
+++ GameClub/app/src/main/java/com/example/install/gameclub/RulesContentFragment.java
@@ -11,6 +11,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 //import static com.example.install.gameclub.R.id.fab;
 
@@ -77,11 +78,38 @@ public class RulesContentFragment extends Fragment {
         sectionPagerAdapter = new SectionPagerAdapter(getChildFragmentManager());
         viewPager = (ViewPager) view.findViewById(R.id.rulecontent);
         viewPager.setAdapter(sectionPagerAdapter);
-        //creating a snackbar to notify user to keep on swiping
-        if(savedInstanceState == null){
-            Snackbar.make(view, "Please swipe left to read through the rules", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
-        }
+        viewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        ImageButton leftButton = (ImageButton) view.findViewById(R.id.left);
+        ImageButton rightButton = (ImageButton) view.findViewById(R.id.right);
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int location = viewPager.getCurrentItem();
+                if(location > 0){
+                    location--;
+                    viewPager.setCurrentItem(location);
+                }
+                else if(location == 0){
+                    viewPager.setCurrentItem(viewPager.getChildCount()+1);
+                }
+            }
+        });
+
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int location = viewPager.getCurrentItem();
+                location++;
+                if(location >= viewPager.getChildCount()+1){
+                    viewPager.setCurrentItem(0);
+                }
+                else{
+                    viewPager.setCurrentItem(location);
+                }
+            }
+        });
+
         return view;
     }
     public class SectionPagerAdapter extends FragmentPagerAdapter {
@@ -92,17 +120,17 @@ public class RulesContentFragment extends Fragment {
             //adding content to the viewpager
             switch(position){
                 case 0:
-                    return RulesFragment.newInstance("Rule #1", "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
+                    return RulesFragment.newInstance("Rule #1",R.drawable.prohibition, "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
                 case 1:
-                    return RulesFragment.newInstance("Rule #2", "Play and carries out activities in a way so that everyone have fun .This involves knowing and following the rules, Staying focused on the game, playing at an appropriate volume level, showing good sportsmanship and making consensus decisions on the games that are played.\n");
+                    return RulesFragment.newInstance("Rule #2",R.drawable.handshake, "Play and carries out activities in a way so that everyone have fun .This involves knowing and following the rules, Staying focused on the game, playing at an appropriate volume level, showing good sportsmanship and making consensus decisions on the games that are played.\n");
                 case 2:
-                    return RulesFragment.newInstance("Rule #3", "Treat the game materials with the utmost respect . This means that treating club's properties like consoles , peripherals, tools ,etc carefully.If you played or handled any club's equipments,make sure to clean up before leaving ");
+                    return RulesFragment.newInstance("Rule #3",R.drawable.respect, "Treat the game materials with the utmost respect . This means that treating club's properties like consoles , peripherals, tools ,etc carefully.If you played or handled any club's equipments,make sure to clean up before leaving ");
                 case 3:
-                    return RulesFragment.newInstance("Rule #4", "Food and drinks are not allowed inside the clubroom due to potential risk of damaging equipments");
+                    return RulesFragment.newInstance("Rule #4",R.drawable.food, "Food and drinks are not allowed inside the clubroom due to potential risk of damaging equipments");
                 case 4:
-                    return RulesFragment.newInstance("Rule #5", "If you break it , you pay for it . Meaning any serious damage caused to equipment will result in the person having to pay back to the club so we can get a replacement");
+                    return RulesFragment.newInstance("Rule #5",R.drawable.money, "If you break it , you pay for it . Meaning any serious damage caused to equipment will result in the person having to pay back to the club so we can get a replacement");
                 default:
-                    return RulesFragment.newInstance("Rule #1", "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
+                    return RulesFragment.newInstance("Rule #1",R.drawable.prohibition, "DO NOT sacrifice your study for the club , club activities should only be taken during your free time.");
             }
         }
         public int getCount(){
@@ -110,6 +138,82 @@ public class RulesContentFragment extends Fragment {
         }
 
     }
+
+public class DepthPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.75f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 0) { // [-1,0]
+            // Use the default slide transition when moving to the left page
+            view.setAlpha(1);
+            view.setTranslationX(0);
+            view.setScaleX(1);
+            view.setScaleY(1);
+
+        } else if (position <= 1) { // (0,1]
+            // Fade the page out.
+            view.setAlpha(1 - position);
+
+            // Counteract the default slide transition
+            view.setTranslationX(pageWidth * -position);
+
+            // Scale the page down (between MIN_SCALE and 1)
+            float scaleFactor = MIN_SCALE
+                    + (1 - MIN_SCALE) * (1 - Math.abs(position));
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
+
+public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+    private static final float MIN_SCALE = 0.85f;
+    private static final float MIN_ALPHA = 0.5f;
+
+    public void transformPage(View view, float position) {
+        int pageWidth = view.getWidth();
+        int pageHeight = view.getHeight();
+
+        if (position < -1) { // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            view.setAlpha(0);
+
+        } else if (position <= 1) { // [-1,1]
+            // Modify the default slide transition to shrink the page as well
+            float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+            float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+            float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+            if (position < 0) {
+                view.setTranslationX(horzMargin - vertMargin / 2);
+            } else {
+                view.setTranslationX(-horzMargin + vertMargin / 2);
+            }
+
+            // Scale the page down (between MIN_SCALE and 1)
+            view.setScaleX(scaleFactor);
+            view.setScaleY(scaleFactor);
+
+            // Fade the page relative to its size.
+            view.setAlpha(MIN_ALPHA +
+                    (scaleFactor - MIN_SCALE) /
+                            (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+        } else { // (1,+Infinity]
+            // This page is way off-screen to the right.
+            view.setAlpha(0);
+        }
+    }
+}
     // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
